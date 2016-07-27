37d32a7b1371a69ddb6197b6659982b6ff7a9506
Agustí Moll <agusti.moll@guifi.net>
2016-07-27 13:41:54 +0200

    rename flow anonymize

---
+++ flowAnonymize.js
@@ -0,0 +1,11 @@
+'use strict'
+const EventEmitter = require('events').EventEmitter;
+
+class flowAnonymize extends EventEmitter{
+  constructor(){
+    super();
+  }
+
+}
+
+module.exports = guifiRange
